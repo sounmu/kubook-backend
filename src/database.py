ef4aaee1e21f134c42a09a7053e1820253d5c382
smreosms13 <smreosms13@gamil.com>
2024-04-20 01:44:12 +0900

    refactor: replace paramiko as sshtunnel and make class SSHConnection

--- src/database.py
+++ src/database.py
@@ -1,5 +1,4 @@
-import paramiko
-import os
+from sshtunnel import SSHTunnelForwarder
 from sqlalchemy import create_engine, text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -10,13 +9,7 @@ settings = Settings()
 ssh_host = settings.SSH_HOST
 ssh_port = settings.SSH_PORT
 ssh_username = settings.SSH_USERNAME
-ssh_key_filename = settings.SSH_KEYFILE  # Update this with the correct path to your key file
-
-if os.access(ssh_key_filename, os.R_OK):
-    print(f"You have permission to read the key file: {ssh_key_filename}")
-else:
-    print(f"You do not have permission to read the key file: {ssh_key_filename}")
-    exit(1)  # Exit the program if you don't have permission to read the key file
+ssh_key_filename = settings.SSH_KEYFILE
 
 db_host = settings.DB_HOST
 db_port = settings.DB_PORT
@@ -24,34 +17,54 @@ db_name = settings.DB_NAME
 db_user = settings.DB_USER
 db_password = settings.DB_PASSWORD
 
-print(f"Connecting to SSH host: {ssh_host}:{ssh_port} with username: {ssh_username}")
-ssh_client = paramiko.SSHClient()
-ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
-ssh_tunnel = None
-
-try:
-    ssh_client.connect(hostname=ssh_host, port=ssh_port, username=ssh_username, key_filename=ssh_key_filename,
-                       timeout=30)
-    print("SSH connection successful.")
-
-    remote_bind_address = (db_host, db_port)
-    local_bind_address = ('localhost', 3307)
-    
-    try:
-        ssh_tunnel = ssh_client.get_transport().open_channel('direct-tcpip', remote_bind_address, local_bind_address )
-        print("SSH tunnel successfully established.")
-    except Exception as e:
-        print(f"Error creating SSH tunnel: {e}")
-        exit(1)
-
-except Exception as e:
-    print(f"Error connecting to SSH: {e}")
-    exit(1)
-
-SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{db_user}:{db_password}@localhost:3307/{db_name}"
+local_host = 'localhost'
+local_port = 3307 
 
 Base = declarative_base()
+
+class SSHConnection:
+    def __init__(self):
+        self.ssh_host = ssh_host
+        self.ssh_port = ssh_port
+        self.ssh_username = ssh_username
+        self.ssh_key_filename = ssh_key_filename
+        self.remote_bind_address = (db_host, db_port)
+        self.local_bind_address = (local_host, local_port)
+        self.ssh_client = None
+
+    def check_ssh_key_permission(self):
+        try:
+            with open(self.ssh_key_filename, 'r'):
+                print(f"You have permission to read the key file: {self.ssh_key_filename}")
+        except PermissionError:
+            print(f"You do not have permission to read the key file: {self.ssh_key_filename}")
+            exit(1)
+
+    def connect(self):
+        print(f"Connecting to SSH host: {self.ssh_host}:{self.ssh_port} with username: {self.ssh_username}")
+        try:
+            self.check_ssh_key_permission()
+            self.ssh_client = SSHTunnelForwarder(
+                self.ssh_host,
+                self.ssh_port,
+                ssh_username=self.ssh_username,
+                ssh_pkey=self.ssh_key_filename,
+                remote_bind_address=self.remote_bind_address,
+                local_bind_address=self.local_bind_address,
+            )
+        except Exception as e:
+            print(f"Error connecting to SSH: {e}")
+            exit(1)
+        else :
+            print("SSH connection successful.")
+            self.ssh_client.start()
+
+    def close(self):
+        # Close the SSH tunnel and client
+        if self.ssh_client:
+            self.ssh_client.stop()
+
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{db_user}:{db_password}@{local_host}:{local_port}/{db_name}"
 
 class EngineConnection:
     def __init__(self):
@@ -63,6 +76,12 @@ class EngineConnection:
 
     def get_connection(self):
         return self.engine.connect()
+    
+    def close_engine(self):
+        return self.engine.dispose()
+
+ssh_connection = SSHConnection()
+ssh_connection.connect() 
 
 def get_db_session():
     engine_conn = EngineConnection()
@@ -77,10 +96,3 @@ with get_db_session() as session:
         for row in server_data:
             print(row)
     else : print('No data')
-
-
-# Close the SSH tunnel and client
-if ssh_tunnel:
-    ssh_tunnel.close()
-if ssh_client:
-    ssh_client.close()
