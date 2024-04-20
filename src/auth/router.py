7e3638fc780d19800e5417feb17e0cfb3d14ca2b
mjkweon17 <mjkweon17@korea.ac.kr>
2024-04-20 17:23:13 +0900

    refactor: modularize login functionality across multiple files

--- src/auth/router.py
+++ src/auth/router.py
@@ -1,13 +1,10 @@
-from datetime import datetime, timedelta, timezone
-from pydantic import BaseModel
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
-from jose import JWTError, jwt
 
-from dependencies import get_db
-from auth.firebase_login import sign_in_with_email_and_password
-from models import User
 from config import Settings
+from dependencies import get_db
+import auth.schemas as auth_schemas
+import auth.service as auth_service
 
 router = APIRouter(
     prefix="/auth",
@@ -17,58 +14,9 @@ router = APIRouter(
 settings = Settings()
 
 
-def create_jwt(
-    data: dict,
-    secret_key: str,
-    algorithm: str,
-    expires_delta: timedelta | None = None
-):
-    """
-    Create a JWT token with the given data.
-
-    Args:
-        data (dict): The data to be encoded in the token.
-        secret_key (str): The secret key used to sign the token.
-        algorithm (str): The algorithm used to sign the token.
-        expires_delta (timedelta | None, optional): The expiration time for the token. Defaults to None, which means the token will expire in 15 minutes.
-
-    Returns:
-        str: The encoded JWT token.
-    """
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.now(timezone.utc) + expires_delta
-    else:
-        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, secret_key, algorithm=algorithm)
-    return encoded_jwt
-
-
-class LoginRequest(BaseModel):
-    email: str
-    password: str
-
-
-class UserInfo(BaseModel):
-    id: int
-    user_name: str
-
-
-class Token(BaseModel):
-    access_token: str
-    refresh_token: str
-    token_type: str
-
-
-class LoginResponse(BaseModel):
-    token: Token
-    user: UserInfo
-
-
 @router.post(
     "/login",
-    response_model=LoginResponse,
+    response_model=auth_schemas.LoginResponse,
     status_code=status.HTTP_200_OK,
     summary="로그인",
     description="""
@@ -88,65 +36,10 @@ class LoginResponse(BaseModel):
     }
 )
 async def login(
-    request: LoginRequest,
+    request: auth_schemas.LoginRequest,
     db: Session = Depends(get_db)
 ):
-    # Authenticate user
-    # Check if user exists in Firebase
-    firebase_response = await sign_in_with_email_and_password(request.email, request.password)
-
-    if firebase_response == "INVALID_EMAIL":
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
-    elif firebase_response == "INVALID_PASSWORD":
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid password")
-    elif firebase_response == "USER_DISABLED":
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="User disabled")
-
-    # Check if user information exists in the DB
-    local_id = firebase_response
-    user = db.query(User).filter(User.auth_id == local_id).first()
-
-    # If user information does not exist in the DB, create a new user
-    if user is None:
-        user = User(
-            auth_id=local_id,
-            user_name="권민재",
-            email=request.email
-        )
-        db.add(user)
-        db.commit()
-        db.refresh(user)
-
-    # Create JWT token
-    # Create Access Token
-    access_token_expires = timedelta(minutes=settings.JWT_ACCESS_EXPIRATION_TIME_MINUTES)
-    access_token = create_jwt(
-        data={"sub": user.id},
-        secret_key=settings.JWT_SECRET_KEY,
-        algorithm=settings.JWT_ALGORITHM,
-        expires_delta=access_token_expires
-    )
-
-    # Create Refresh Token
-    refresh_token_expires = timedelta(minutes=settings.JWT_REFRESH_EXPIRATION_TIME_MINUTES)
-    refresh_token = create_jwt(
-        data={"sub": user.id},
-        secret_key=settings.JWT_SECRET_KEY,
-        algorithm=settings.JWT_ALGORITHM,
-        expires_delta=refresh_token_expires
-    )
-
-    return {
-        "token": {
-            "access_token": access_token,
-            "refresh_token": refresh_token,
-            "token_type": "bearer"
-        },
-        "user": {
-            "id": user.id,
-            "user_name": user.user_name
-        }
-    }
+    return await auth_service.login(request, db)
 
 
 @router.post(
--- src/auth/service.py
+++ src/auth/service.py
@@ -1,1 +1,77 @@
-from sqlalchemy.orm import create_session
+from datetime import timedelta
+
+
+from sqlalchemy.orm import Session
+from fastapi import HTTPException, status
+
+from config import Settings
+from models import User
+from auth.firebase import sign_in_with_email_and_password
+from auth.token_service import create_jwt
+
+
+async def login(request, db: Session):
+    # Authenticate user
+    # Check if user exists in Firebase
+    firebase_response = await sign_in_with_email_and_password(request.email, request.password)
+    try:
+        localId = firebase_response["localId"]
+        return localId
+    except KeyError:
+        error_message = firebase_response["error"]["message"]
+        if error_message == "INVALID_EMAIL":
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+        elif error_message == "INVALID_PASSWORD":
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid password")
+        elif error_message == "USER_DISABLED":
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="User disabled")
+
+    # Check if user information exists in the DB
+    local_id = firebase_response
+    user = db.query(User).filter(User.auth_id == local_id).first()
+
+    # If user information does not exist in the DB, create a new user
+    if user is None:
+        user = User(
+            auth_id=local_id,
+            user_name="권민재",
+            email=request.email
+        )
+        db.add(user)
+        db.commit()
+        db.refresh(user)
+
+
+# TODO: create jwt token을 token_service.py에 옮기고, 간략화할 것
+# TODO: 회원가입 API 작성하기
+
+    # Create JWT token
+    # Create Access Token
+    access_token_expires = timedelta(minutes=Settings().JWT_ACCESS_EXPIRATION_TIME_MINUTES)
+    access_token = create_jwt(
+        data={"sub": user.id},
+        secret_key=Settings().JWT_SECRET_KEY,
+        algorithm=Settings().JWT_ALGORITHM,
+        expires_delta=access_token_expires
+    )
+
+    # Create Refresh Token
+    refresh_token_expires = timedelta(minutes=Settings().JWT_REFRESH_EXPIRATION_TIME_MINUTES)
+    refresh_token = create_jwt(
+        data={"sub": user.id},
+        secret_key=Settings().JWT_SECRET_KEY,
+        algorithm=Settings().JWT_ALGORITHM,
+        expires_delta=refresh_token_expires
+    )
+
+    return {
+        "token": {
+            "access_token": access_token,
+            "refresh_token": refresh_token,
+            "token_type": "bearer"
+        },
+        "user": {
+            "id": user.id,
+            "user_name": user.user_name
+        }
+    }
