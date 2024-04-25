f2d5dd6f093f4ab51869095b5dcfff5896d1a51c
mjkweon17 <mjkweon17@korea.ac.kr>
2024-04-25 18:35:59 +0900

    feat: update models.py to align with revised database schema

--- src/models.py
+++ src/models.py
@@ -196,5 +196,7 @@ class LibrarySetting(Base):
     name = Column(String(50), nullable=False)
     value = Column(String(50), nullable=False)
     data_type = Column(String(50), nullable=False)
-    description = Column(Text)
+    description = Column(Text, nullable=True)
+    created_at = Column(DateTime, nullable=False, default=func.current_timestamp())
+    updated_at = Column(DateTime, nullable=False, default=func.current_timestamp(), onupdate=func.current_timestamp())
     is_valid = Column(Boolean, nullable=False, default=True)
