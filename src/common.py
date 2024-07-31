2e93c904a9560fa9708e3282d0f27e21c5f5e3a2
minjaekweon <mjkweon17@korea.ac.kr>
2024-07-31 21:47:38 +0900

    feat: . 추가함

--- src/common.py
+++ src/common.py
@@ -1,10 +1,10 @@
 from typing import List, Type
 from pydantic import BaseModel, Field, create_model
- 
+
 
 class CustomBaseModel(BaseModel):
     """
-        기존 BaseModel에 custom classmethod 추가
+        기존 BaseModel에 custom classmethod 추가.
         all_optional : 모든 속성 타입을 Optional로 변환
         omit_fields : 기존 클래스에서 특정 field들을 제거한 클래스를 반환
     """
@@ -20,14 +20,14 @@ class CustomBaseModel(BaseModel):
             __base__=cls,
             **{
                 name: (
-                    info.annotation, 
+                    info.annotation,
                     Field(None, title=info.title, description=info.description, examples=info.examples)
                 ) for name, info in cls.model_fields.items()
             }
         )
-    
+
     @classmethod
-    def omit_fields(cls, name:str, attrs: List[str]) -> Type["CustomBaseModel"]:
+    def omit_fields(cls, name: str, attrs: List[str]) -> Type["CustomBaseModel"]:
         """
         Creates a new model with the omitted fields.
 
@@ -35,9 +35,9 @@ class CustomBaseModel(BaseModel):
         """
         new_annotations = {
             name: (
-                    info.annotation, 
-                    Field(info.default, title=info.title, description=info.description, examples=info.examples)
-                ) for name, info in cls.model_fields.items() if name not in attrs
+                info.annotation,
+                Field(info.default, title=info.title, description=info.description, examples=info.examples)
+            ) for name, info in cls.model_fields.items() if name not in attrs
         }
         return create_model(name, __base__=CustomBaseModel, **new_annotations)
 
