9c4d14a4881f318ccba062fe71c1b548f632c1f4
임유나 <youna0104@naver.com>
2024-08-16 23:41:30 +0900

    feat: user_book_request_route.py - route에서 user의 도서 구매 요청 수정 api 구현 - domain.services.user_service.py 구현 필요

--- src/routes/user_book_request_route.py
+++ src/routes/user_book_request_route.py
@@ -3,9 +3,10 @@ from typing import List
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 
-import domain.schemas.admin_schemas as s
+from routes.request.update_book_request_request import UpdateBookRequest as req
+from routes.response.book_request_response import BookRequest as res
 from dependencies import get_current_active_user, get_db
-from domain.services.admin_service import *
+from domain.services.user_service import *
 from repositories.requested_book import RequestedBook
 
 router = APIRouter(
@@ -18,8 +19,8 @@ router = APIRouter(
 @router.put(
     "/{user_id}/book-requests/{requests_id}",
     summary="도서 구매 요청 수정",
-    response_model=s.BookRequest,
+    response_model=res,
     status_code=status.HTTP_200_OK
 )
-async def update_book_request(request_id: int, request_data: BookRequestUpdate, db: Session = Depends(get_db), get_current_active_user=Depends(get_current_active_user)):
-    return update_item(RequestedBook, request_id, request_data, db)
+async def update_user_book_request(user_id: int, request_id: int, request_data: req, db: Session = Depends(get_db), get_current_active_user=Depends(get_current_active_user)):
+    return update_item(RequestedBook, request_id, request_data, db)  # user_service.py 미개발
