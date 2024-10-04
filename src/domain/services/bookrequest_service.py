f6d40825eb7f52fd737c09fa4d135df5e3918237
임유나 <youna0104@naver.com>
2024-10-04 17:36:48 +0900

    fix: update_item 대체

--- src/domain/services/bookrequest_service.py
+++ src/domain/services/bookrequest_service.py
@@ -10,24 +10,57 @@ from domain.schemas.bookrequest_schemas import (
     DomainResBookRequest,
 )
 from repositories.models import RequestedBook
-from utils.crud_utils import delete_item, update_item
+from utils.crud_utils import delete_item
 
 
 async def service_update_bookrequest(request_data: DomainReqPutBookRequest, db: Session):
-    updated_book = update_item(RequestedBook, request_data.request_id, request_data, db)
-    # domain response schema 생성
-    response = DomainResBookRequest(
-        user_id=updated_book.user_id,
-        request_id=updated_book.id,
-        book_title=updated_book.book_title,
-        publication_year=updated_book.publication_year,
-        request_link=updated_book.request_link,
-        reason=updated_book.reason,
-        processing_status=updated_book.processing_status,
-        request_date=updated_book.requested_at.date(),
-        reject_reason=updated_book.reject_reason
-    )
-    return response
+    stmt = (select(RequestedBook).where(
+            and_(RequestedBook.id==request_data.request_id,
+                 RequestedBook.user_id==request_data.user_id,
+                 RequestedBook.is_deleted==False))
+            .order_by(RequestedBook.updated_at))
+    try:
+        item = db.execute(stmt).scalar_one()
+        if not item:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="Requested book not found")
+        request_book = item.__dict__
+        updated_book = request_data.__dict__
+        for key, value in updated_book.items():
+            if value is not None and key in request_book:
+                if isinstance(value, type(request_book[key])):
+                    setattr(item, key, value)
+                else:
+                    raise HTTPException(
+                        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                        detail=f"Invalid value type for column {key}. Expected {type(request_book[key])}, got {type(value)}."
+                    )
+        db.add(item)
+        db.flush()
+    except HTTPException as e:
+        db.rollback()
+        raise e
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail=f"Unexpected error occurred during update: {str(e)}") from e
+    else:
+        db.commit()
+        db.refresh(item)
+        # domain response schema 생성
+        response = DomainResBookRequest(
+        user_id=item.user_id,
+        request_id=item.id,
+        book_title=item.book_title,
+        publication_year=item.publication_year,
+        request_link=item.request_link,
+        reason=item.reason,
+            processing_status=item.processing_status,
+            request_date=item.requested_at.date(),
+            reject_reason=item.reject_reason
+        )
+        return response
+
 
 
 async def service_read_bookrequest(request_data: DomainReqGetBookRequest, db: Session) -> list[DomainResBookRequest]:
