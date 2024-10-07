04fb437a90a336d3d4552410a2c154998389c4a2
sounmu <hyginus011111@gmail.com>
2024-10-07 12:11:53 +0900

    feat: search_books (#49)
    * feat: 도서 검색 추가
    
    * feat: 키워드에 와일드카드 추가 및 성능 개선
    
    * fix: delete create, update  utils in review&loan service
    
    * feat: 리뷰받은 수정사항, image_url 추가
    
    ---------
    
    Co-authored-by: smreosms13 <76930385+smreosms13@users.noreply.github.com>

--- src/domain/services/loan_service.py
+++ src/domain/services/loan_service.py
@@ -5,29 +5,23 @@ from fastapi import HTTPException, status
 from sqlalchemy import and_, select
 from sqlalchemy.orm import Session
 
-from domain.schemas.loan_schemas import DomainReqPostLoan, DomainReqPutLoan, DomainResGetLoan, DomianResGetLoanItem
+from domain.schemas.loan_schemas import (
+    DomainReqPostLoan,
+    DomainReqPutLoan,
+    DomainResGetLoan,
+    DomianResGetLoanItem,
+)
 from repositories.models import Book, Loan
-from utils.crud_utils import get_item, update_item
+from utils.crud_utils import get_item
 
 
 async def service_read_loans_by_user_id(user_id, db: Session):
-    stmt = (
-        select(Loan)
-        .where(
-            and_(
-                Loan.user_id == user_id,
-                Loan.is_deleted == False
-            )
-        ).order_by(Loan.updated_at)
-    )
+    stmt = select(Loan).where(and_(Loan.user_id == user_id, Loan.is_deleted == False)).order_by(Loan.updated_at)
 
     try:
         loans = db.scalars(stmt).all()  # loans를 리스트로 반환
         if not loans:
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail="Loans not found"
-                ) from None
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Loans not found") from None
         result = [
             DomainResGetLoan(
                 loan_id=loan.id,
@@ -38,15 +32,15 @@ async def service_read_loans_by_user_id(user_id, db: Session):
                 extend_status=loan.extend_status,
                 overdue_days=loan.overdue_days,
                 return_status=loan.return_status,
-                return_date=loan.return_date
+                return_date=loan.return_date,
             )
             for loan in loans
         ]
     except Exception as e:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=f"Unexpected error occurred during retrieve: {str(e)}"
-            ) from e
+            detail=f"Unexpected error occurred during retrieve: {str(e)}",
+        ) from e
     return result
 
 
@@ -55,77 +49,29 @@ async def service_extend_loan(request: DomainReqPutLoan, db: Session):
 
     if loan.user_id != request.user_id:
         raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail="You do not have permission to access this loan."
-            )
+            status_code=status.HTTP_403_FORBIDDEN, detail="You do not have permission to access this loan."
+        )
     # 이미 반납된 도서인지 확인
     if loan.return_status:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-           detail="This loan has already been returned."
-            )
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="This loan has already been returned.")
     # 이미 연장된 도서인지 확인
     if loan.extend_status:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="This loan has already been extended."
-            )
-
-    dict = {"due_date": loan.due_date + timedelta(days=7), "extend_status": True, "updated_at": _datetime.now()}
-
-    updated_loan = update_item(Loan, request.loan_id, dict, db)
-
-    result = DomianResGetLoanItem(
-        loan_id=updated_loan.id,
-        book_id=updated_loan.book_id,
-        user_id=updated_loan.user_id,
-        created_at=updated_loan.created_at,
-        updated_at=updated_loan.updated_at,
-        loan_date=updated_loan.loan_date,
-        due_date=updated_loan.due_date,
-        extend_status=updated_loan.extend_status,
-        overdue_days=updated_loan.overdue_days,
-        return_status=updated_loan.return_status,
-        return_date=updated_loan.return_date
-    )
-
-    return result
-
-
-async def service_create_loan(request: DomainReqPostLoan, db: Session):
-    stmt = (
-        select(Book)
-        .where(
-            Book.id == request.book_id
-        )
-    )
-    valid_book_id = db.execute(stmt).scalar_one_or_none()
-
-    if not valid_book_id:
-        raise HTTPException(
-            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-            detail="Invalid book ID"
-        )
-
-    loan = Loan(
-        book_id=request.book_id,
-        user_id=request.user_id,
-        loan_date=_datetime.today().date(),
-        due_date=(_datetime.today() + timedelta(days=14)).date(),
-        created_at=_datetime.now(),
-        updated_at=_datetime.now()
-    )
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="This loan has already been extended.")
 
     try:
-        db.add(loan)
+        loan.due_date += timedelta(days=7)
+        loan.extend_status = True
+        loan.updated_at = _datetime.now()
+
         db.flush()
 
     except Exception as e:
         db.rollback()
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=f"Unexpected error occurred: {str(e)}"
-            ) from e
+            detail=f"Unexpected error occurred during update: {str(e)}",
+        ) from e
+
     else:
         db.commit()
         db.refresh(loan)
@@ -141,6 +87,52 @@ async def service_create_loan(request: DomainReqPostLoan, db: Session):
             extend_status=loan.extend_status,
             overdue_days=loan.overdue_days,
             return_status=loan.return_status,
-            return_date=loan.return_date
+            return_date=loan.return_date,
+        )
+
+    return result
+
+
+async def service_create_loan(request: DomainReqPostLoan, db: Session):
+    stmt = select(Book).where(Book.id == request.book_id)
+    valid_book_id = db.execute(stmt).scalar_one_or_none()
+
+    if not valid_book_id:
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="Invalid book ID")
+
+    loan = Loan(
+        book_id=request.book_id,
+        user_id=request.user_id,
+        loan_date=_datetime.today().date(),
+        due_date=(_datetime.today() + timedelta(days=14)).date(),
+        created_at=_datetime.now(),
+        updated_at=_datetime.now(),
+    )
+
+    try:
+        db.add(loan)
+        db.flush()
+
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Unexpected error occurred: {str(e)}"
+        ) from e
+    else:
+        db.commit()
+        db.refresh(loan)
+
+        result = DomianResGetLoanItem(
+            loan_id=loan.id,
+            book_id=loan.book_id,
+            user_id=loan.user_id,
+            created_at=loan.created_at,
+            updated_at=loan.updated_at,
+            loan_date=loan.loan_date,
+            due_date=loan.due_date,
+            extend_status=loan.extend_status,
+            overdue_days=loan.overdue_days,
+            return_status=loan.return_status,
+            return_date=loan.return_date,
         )
         return result
--- src/routes/books_route.py
+++ src/routes/books_route.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, Query, status
 from sqlalchemy.orm import Session
 
 from dependencies import get_db
 from domain.schemas.bookinfo_schemas import ReqeustGetBookInfo
+from domain.services.book_service import service_search_books
 from domain.services.bookinfo_service import read_bookinfo as service_read_bookinfo
+from routes.response.book_response import RouteResGetBookList
 from routes.response.bookinfo_response import BookInfoResponse
 
 router = APIRouter(
@@ -38,4 +40,24 @@ async def get_bookinfo(
         major=domain_res.major,
         language=domain_res.language
     )
+
+    return result
+
+
+@router.get(
+    "",
+    summary="도서 검색",
+    response_model=RouteResGetBookList,
+    status_code=status.HTTP_200_OK
+)
+async def search_books(
+    searching_keyword: str = Query(alias="search"),
+    db: Session = Depends(get_db)
+):
+    domain_res = await service_search_books(searching_keyword, db)
+    result = RouteResGetBookList(
+        data=domain_res,
+        count=len(domain_res)
+    )
+
     return result
