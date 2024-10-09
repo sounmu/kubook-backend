c4ba85a82e3b507c1c3291dc83e4608bd68ecf85
ywnjeong <wjddusdn1237@gmail.com>
2024-10-09 11:07:33 +0900

    fix: check valid user id

--- src/routes/bookrequest_route.py
+++ src/routes/bookrequest_route.py
@@ -2,7 +2,9 @@ from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 
 from dependencies import get_current_active_user, get_db
-from domain.schemas.bookrequest_schemas import RouteReqPostBookRequest, RouteResPostBookRequest
+from domain.schemas.bookrequest_schemas import DomainReqPostBookRequest
+from routes.request.bookrequest_request import RouteReqPostBookRequest
+from routes.response.bookrequest_response import RouteResPostBookRequest
 from domain.services.bookrequest_service import service_create_bookrequest
 
 router = APIRouter(
@@ -19,16 +21,29 @@ router = APIRouter(
     summary="구매 요청"
 )
 async def create_book_request(
-    PurchaseCreate: RouteReqPostBookRequest,
+    purchase_create: RouteReqPostBookRequest,
     db: Session = Depends(get_db),
     current_user=Depends(get_current_active_user)
 ):
-    domain_req = RouteReqPostBookRequest(
+    domain_req = DomainReqPostBookRequest(
         user_id=current_user.id,
-        book_title=PurchaseCreate.book_title,
-        publication_year=PurchaseCreate.publication_year,
-        request_link=PurchaseCreate.request_link,
-        reason=PurchaseCreate.reason
+        book_title=purchase_create.book_title,
+        publication_year=purchase_create.publication_year,
+        request_link=purchase_create.request_link,
+        reason=purchase_create.reason
     )
+
     result = await service_create_bookrequest(domain_req, db)
+
+    result = RouteResPostBookRequest(
+        request_id=result.request_id,
+        user_id=result.user_id,
+        book_title=result.book_title,
+        publication_year=result.publication_year,
+        request_link=result.request_link,
+        requested_at=result.requested_at,
+        reason=result.reason,
+        processing_status=result.processing_status
+    )
+
     return result
