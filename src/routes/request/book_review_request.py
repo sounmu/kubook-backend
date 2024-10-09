e52fa5b7a22c19084c86b4abd44bd5b5400ba0a9
smreosms13 <smreosms13@gmail.com>
2024-10-09 14:06:29 +0900

    fix: apply new book table to book review routes, service, schema

--- src/routes/request/book_review_request.py
+++ src/routes/request/book_review_request.py
@@ -3,8 +3,11 @@ from pydantic import BaseModel, Field
 
 class RouteReqPutReview(BaseModel):
     review_content: str = Field(title="review_content", description="리뷰 내용")
-#BookReviewUpdateRouteRequest
+
+
+# BookReviewUpdateRouteRequest
+
 
 class RouteReqPostReview(BaseModel):
-    book_info_id: int = Field(title="book_info_id", description="리뷰한 책 정보 ID", example=1, gt=0)
+    book_id: int = Field(title="book_id", description="리뷰한 책 ID", example=1, gt=0)
     review_content: str = Field(title="review_content", description="리뷰 내용")
